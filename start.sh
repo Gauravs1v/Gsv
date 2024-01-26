5cd05001fa6a20e0eced98373b4c785551e63e81
Gauravs1v <157791122+Gauravs1v@users.noreply.github.com>
2024-01-26 14:12:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/url-Shortner- /url-Shortner-
+  git clone auto-filter-bot/auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /url-Shortner-
+  git clone $UPSTREAM_REPO /auto-filter-bot
 fi
-cd /url-Shortner-
+cd /auto-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
